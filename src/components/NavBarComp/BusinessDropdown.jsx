0c183c06bc927f9f33f3a41a04b5ffddd612fba7
Ronin <compyneller@gmail.com>
2022-09-08 18:04:44 +0530

    rate-fees

--- src/components/NavBarComp/BusinessDropdown.jsx
+++ src/components/NavBarComp/BusinessDropdown.jsx
@@ -5,7 +5,7 @@ const BusinessDropdown = () => {
   return (
     <NavDropdown title="Business" id="collasible-nav-dropdown">
       <NavDropdown.Item href="/how-to-use">How to use</NavDropdown.Item>
-      <NavDropdown.Item href="/rate-fee">Rate & Fees</NavDropdown.Item>
+      <NavDropdown.Item href="/rate-fees">Rate & Fees</NavDropdown.Item>
       <NavDropdown.Item href="/security">Security</NavDropdown.Item>
       <NavDropdown.Item href="/reviews">Reviews</NavDropdown.Item>
       <NavDropdown.Item href="/refer">Refer Friend</NavDropdown.Item>
